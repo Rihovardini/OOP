ec9d61d8080c321811251dae50c550a5095cb4a1
Ihor Andriiv <andriivihor@icloud.com>
2018-08-15 14:10:36 +0300

    replaced for in with Object.keys

--- task3/main.js
+++ task3/main.js
@@ -1,72 +1,67 @@
 
-let form  = document.forms.namedItem("fileinfo"),
-    users=[],
-    count=0,
-    tBody=document.createElement('tbody');
-    form.addEventListener('submit',(e)=>{
-    e.preventDefault();
-    let data = new FormData(form),
-        arrData=[];
-    for (let value of data.entries()) { 
-        arrData.push(value);
-    }
-     let user= new User(arrData);
-     users.push(user);
-    
-    createRow(user);
+let form = document.forms.namedItem("fileinfo"),
+  users = [],
+  count = 0,
+  tBody = document.createElement("tbody");
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let data = new FormData(form),
+    arrData = [];
+  for (let value of data.entries()) {
+    arrData.push(value);
+  }
+  let user = new User(arrData);
+  users.push(user);
+
+  createRow(user);
 });
 
-function createRow(value){
-    const tr=document.createElement('tr'),
-          table=document.querySelector('table');
-          tr.setAttribute('data-index',count);
-          tBody.appendChild(tr);
-          for(let key in value){
-              const td=document.createElement('td');
-              if(key!="isDataVisible"){
-                td.textContent=value[key]?value[key]:"Not found";
-                tr.appendChild(td);
-            }
-          }
-          createEventClick(tr);
-          table.appendChild(tBody);
-          count++;
+function createRow(value) {
+  const tr = document.createElement("tr"),
+    table = document.querySelector("table");
+  tr.setAttribute("data-index", count);
+  tBody.appendChild(tr);
+  let arr = Object.keys(value);
+  arr.pop();
+  arr.forEach(key => {
+    const td = document.createElement("td");
+    td.textContent = value[key] ? value[key] : "Not found";
+    tr.appendChild(td);
+  });
+
+  createEventClick(tr);
+  table.appendChild(tBody);
+  count++;
 }
-class SuperUser{
-        changeDataVisibility(tag){
-            debugger
-            if(this.isDataVisible==true){
-                for(let i=1;i<tag.length;i++){
-                    tag[i].style.display='none';
-                }
-                this.isDataVisible=false;
-            }
-            else{
-                for(let i=1;i<tag.length;i++){
-                    tag[i].style.display='table-cell';
-                }
-                this.isDataVisible=true;
-            }
-        }
-       
+class SuperUser {
+  changeDataVisibility(tag) {
+    if (this.isDataVisible == true) {
+      for (let i = 1; i < tag.length; i++) {
+        tag[i].style.display = "none";
+      }
+      this.isDataVisible = false;
+    } else {
+      for (let i = 1; i < tag.length; i++) {
+        tag[i].style.display = "table-cell";
+      }
+      this.isDataVisible = true;
     }
+  }
+}
 
-
-class User extends SuperUser{
-    constructor(data){
-        super(data)
-        for(let i=0;i<data.length;i++){
-            this[data[i][0]]=data[i][1];
-        }
-        this.isDataVisible=true;
+class User extends SuperUser {
+  constructor(data) {
+    super(data);
+    for (let i = 0; i < data.length; i++) {
+      this[data[i][0]] = data[i][1];
     }
+    this.isDataVisible = true;
+  }
 }
 
-function createEventClick(tag){
-    tag.addEventListener('click',function(e){
-        let index=tag.getAttribute("data-index");
-        users[index].changeDataVisibility(tag.children);
-    })
+function createEventClick(tag) {
+  tag.addEventListener("click", function(e) {
+    let index = tag.getAttribute("data-index");
+    users[index].changeDataVisibility(tag.children);
+  });
 }
-
-
