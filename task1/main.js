fe3691a1708bee0b77e02632d132ae3277a851d0
Ihor Andriiv <andriivihor@icloud.com>
2018-08-16 09:58:01 +0300

    Changed methods and variables in classes

--- task1/main.js
+++ task1/main.js
@@ -1,3 +1,4 @@
+// ES 6 implementation
 // class SpaceShip{
 //     constructor(data){
 //         this.name=data.name;
@@ -85,25 +86,68 @@ function SpaceShip(data){
     this.getColor=function(){
         return `${this.color} is big`;
     }
-};
+}
+
 function Corvette(data){
     SpaceShip.call(this,data);
     this.model=data.model;
-    this.height=data.height;
-    this.weight=data.weight;
+    this.classOf=data.classOf;
+    this.assembly=data.assembly;
 
-    this.getName=function(){
-        return `${this.name} is big`; 
+    this.classOfCorv=function(){
+        return `${this.classOf} `; 
     }
 
-    this.getYear=function(){
-        return `${this.year} is big`; 
+    this.getAssembly=function(){
+        return `${this.assembly} `; 
     }
 
-    this.getColor=function(){
-        return `${this.color} is big`; 
+    this.getModel=function(){
+        return `${this.model}`; 
     }
 }
+
+function Frigate(data){
+    SpaceShip.call(this,data);
+    this.operator=data.operator;
+    this.status=data.status;
+    this.builder=data.builder;
+
+    this.getOperator=function(){
+        return `The operator is: ${this.operator}`; 
+    }
+
+    this.getStatus=function(){
+        return `The status is:${this.status} `; 
+    }
+
+    this.getBuilder=function(){
+        return `The builder is ${this.builder}`; 
+    }
+}
+
+function Cutter(data){
+    SpaceShip.call(this,data);
+    this.price=data.price;
+    this.length=data.length;
+    this.speed=data.speed;
+}
+
+Cutter.prototype={
+
+    getPrice:function(){
+        return `The price is: ${this.price}`;
+    },
+
+    getLength:function(){
+        return `The length is: ${this.length}`; 
+    },
+
+    getSpeed:function(){
+        return `The speed is:${this.speed}`; 
+    }
+
+}
 let space=new SpaceShip({name:"Lil",year:"1809",color:"blue"});
-let corvet=new Corvette({name:"Falcon",year:"2809",color:"red",model:'X',height:"120m",weight:"200p"});
+let corvet=new Corvette({name:"Falcon",year:"2809",color:"red",classOf:'X',assembly:"120m",model:"200p"});
 console.log(space,corvet);
